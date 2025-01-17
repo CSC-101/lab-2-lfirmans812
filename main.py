8041477ffbe3854c409e79c685185365297da589
Larissa Firmansyah <lfirs812@gmail.com>
2025-01-17 10:35:36 -0800

    This is me doing lab 2, task 4.

--- main.py
+++ main.py
@@ -46,4 +46,21 @@ second = checked_accesss([1, 0 ,1], 2)    #What is the value of second? 1
 print(second)
 
 
+def length_sum(L: list[str]) -> int:
+   if len(L) > 2:
+      result = len(L[0]) + len(L[1]) + len(L[2])  # For which call below is this statement evaluated. This statement is being evaluated when
+   elif len(L) > 1:  # and what are the values being added?
+      result = len(L[0]) + len(L[1])  # For which call below is this statement evaluated
+   elif len(L) > 0:  # and what are the values being added?
+      result = len(L[0])  # For which call below is this statement evaluated
+   else:  # and what are the values being added?
+      result = 0
+   return result
+
+
+first = length_sum(["this", "is", "the", "first", "call"])
+second = length_sum(["second call"])
+third = length_sum(["another", "call"])
+print()
+
 
